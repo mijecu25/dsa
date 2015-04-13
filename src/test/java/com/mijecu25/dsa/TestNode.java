3a252f6d8924482f5d354bb97263c478ae8f333c
Miguel Velez <miguelvelez@mijecu25.com>
2015-04-12 20:06:52 -0500

    Fixing compile

--- src/test/java/com/mijecu25/dsa/TestNode.java
+++ src/test/java/com/mijecu25/dsa/TestNode.java
@@ -7,9 +7,9 @@ public class TestNode {
 
 	@Test
 	public void TestNodeConstructor() {
-		Node node = new Node(5, null);
+		Node node = new Node(6, null);
 		 
-		Assert.assertSame(node.getClass(), Node.class);
+		Assert.assertSame(node.getClass(), Node.class); 
 	}
 
 }
