ae140db5ecdaffbbd480549f82f7d472ba2a1885
Miguel Velez <miguelvelez@mijecu25.com>
2015-04-15 11:58:45 -0500

    Implemented and tested a few methods

--- src/main/java/com/mijecu25/dsa/DataStructures/LinkedList.java
+++ src/main/java/com/mijecu25/dsa/DataStructures/LinkedList.java
@@ -1,5 +1,72 @@
 package com.mijecu25.dsa.DataStructures;
 
+import com.mijecu25.dsa.Node;
+import com.mijecu25.dsa.Exception.LinkedListUnderflowException;
+
+/**
+ * This is the LinkedList class. It is the simplest implementation of a Linked List.
+ * 
+ * @author Miguel Velez
+ * @version 0.1
+ */
 public class LinkedList {
+	
+	private Node head;
+	
+	/**
+	 * Constructs an empty linked list.
+	 */
+	public LinkedList() {
+		this.head = null;
+	}
+	
+	/**
+	 * Constructs a linked list with the {@code head} node as the head of the list.
+	 * 
+	 * @param head-the node that is the head of the list
+	 */
+	public LinkedList(Node head) {
+		this.head = head;
+	}
+	
+	/**
+	 * Gets but does not remove the head of the linked list.
+	 * 
+	 * @return a reference of the head node.
+	 */
+	public Node peek() {
+		return this.head;
+	}
+	
+	/**
+	 * Returns and removes the head of the linked list.
+	 * 
+	 * @return a reference of the head node.
+	 */
+	public Node getHead() {
+		// Check if the linked list is empty
+		if(this.isEmpty()) {
+			// Throw exception
+			throw new LinkedListUnderflowException();
+		}
+		
+		// Get the head
+		Node removed = this.head;
+		
+		// Make the head of the linked list the next node of the head
+		this.head = this.head.getNext();
+		
+		// Return the previous head
+		return removed;
+	}
+	
+	/**
+	 * Checks if the linked list is empty.
+	 * 
+	 * @return <i>true</i> if the linked list is empty, else <i>false</i>.
+	 */
+	public boolean isEmpty() {
+		return (this.head == null);
+	}
 
 }
--- src/test/java/com/mijecu25/dsa/DataStructures/TestLinkedList.java
+++ src/test/java/com/mijecu25/dsa/DataStructures/TestLinkedList.java
@@ -1,5 +1,88 @@
 package com.mijecu25.dsa.DataStructures;
 
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.internal.runners.statements.ExpectException;
+import org.junit.rules.ExpectedException;
+
+import com.mijecu25.dsa.Node;
+import com.mijecu25.dsa.Exception.LinkedListUnderflowException;
+
+/**
+ * This is the LinkedList test class.
+ * 
+ * @author Miguel Velez
+ * @version 0.1
+ */
 public class TestLinkedList {
+	private Node 		head = new Node(5, null);
+	private LinkedList 	list = new LinkedList(head);
+	private LinkedList	empty = new LinkedList();
+	
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	
+	/**
+	 * Test the constructors.
+	 */
+	@Test
+	public void TestConstructors() {
+		// Create link list
+		LinkedList list = new LinkedList();
+		
+		// Check if it was created
+		Assert.assertNotNull(list);
+		
+		// Create a node
+		Node head = new Node(5, null);
+		
+		// Create a linked list
+		list = new LinkedList(head);
+		
+		//Check if it was created
+		Assert.assertNotNull(list);
+		
+	}
+	
+	/**
+	 * Test if the linked list is empty
+	 */
+	@Test
+	public void testIsEmpty() {
+		// Check if the linked list is not empyt
+		Assert.assertEquals(false, this.list.isEmpty());
+		
+		// Check that it is empty
+		Assert.assertNotEquals(false, this.empty.isEmpty());
+	}
+	
+	/**
+	 * Test the peek method
+	 */
+	@Test
+	public void testPeek() {
+		// Check that there is a head
+		Assert.assertNotNull(this.list.peek());
+		
+		// Check that there is not a head
+		Assert.assertNull(this.empty.peek());
+	}
+
+	/**
+	 * Test the get head method.
+	 */
+	@Test
+	public void testGetHead() {
+		// Expect exception
+		expectedException.expect(LinkedListUnderflowException.class);
+		
+		// Get the head from empty lisr
+		this.empty.getHead();
+		
+		// Check that the head is not null
+		Assert.assertNotNull(this.list.getHead());
+	}
+	
 
 }
