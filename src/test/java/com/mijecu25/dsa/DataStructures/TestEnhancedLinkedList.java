f40e537bd442d80ecd102fa397c2b362a4d189a9
Miguel Velez <miguelvelez@mijecu25.com>
2015-04-19 20:51:12 -0500

    Initial commit of EnhancedLinkedList

---
+++ src/test/java/com/mijecu25/dsa/DataStructures/TestEnhancedLinkedList.java
@@ -0,0 +1,11 @@
+package com.mijecu25.dsa.DataStructures;
+
+/**
+ * This is the EnhancedLinkedList test class.
+ * 
+ * @author Miguel Velez
+ * @version 0.1
+ */
+public class TestEnhancedLinkedList {
+	
+}
