a8dc91a93fba5ad32b1de304191ae34f04ec4107
Miguel Velez <miguelvelez@mijecu25.com>
2015-05-27 10:53:06 -0500

    Added QueueUnderflowException

---
+++ src/main/java/com/mijecu25/dsa/Exception/QueueUnderflowException.java
@@ -0,0 +1,13 @@
+package com.mijecu25.dsa.Exception;
+
+/**
+ * QueueUnderflowException
+ * 
+ * @author Miguel Velez
+ * @version 0.1
+ */
+public class QueueUnderflowException extends RuntimeException 
+{
+	private static final long serialVersionUID = 4128603798523826231L;
+
+}
