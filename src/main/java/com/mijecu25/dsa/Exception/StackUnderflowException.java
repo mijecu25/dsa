b50216f8722539f50db6e0bc7ae37bd2c8419b0a
Miguel Velez <miguelvelez@mijecu25.com>
2015-07-21 12:05:41 -0400

    Initial commit of StackUnderflowException

---
+++ src/main/java/com/mijecu25/dsa/Exception/StackUnderflowException.java
@@ -0,0 +1,13 @@
+package com.mijecu25.dsa.Exception;
+
+/**
+ * StackUnderflowException meaning that the stack is empty.
+ * 
+ * @author Miguel Velez
+ * @version 0.1.2.1
+ */
+public class StackUnderflowException extends RuntimeException {
+
+	private static final long serialVersionUID = -8953227319152343757L;
+
+}
