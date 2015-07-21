7efc03850c19916c97c2d4a81db21c05c8e89e63
Miguel Velez <miguelvelez@mijecu25.com>
2015-07-21 11:58:43 -0400

    Initial commit of LinkedListStack

---
+++ src/main/java/com/mijecu25/dsa/DataStructures/LinkedListStack.java
@@ -0,0 +1,14 @@
+package com.mijecu25.dsa.DataStructures;
+
+/**
+ * Stack implementation using a LinkedList.
+ * 
+ * @author Miguel Velez
+ * @version 0.1.2.1
+ */
+public class LinkedListStack {
+	
+	private LinkedList linkedList;
+	
+	
+}
