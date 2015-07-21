4a576fb0beed1d438c90d3eba727b62536ee88e4
Miguel Velez <miguelvelez@mijecu25.com>
2015-07-21 12:14:37 -0400

    Extending LLUnderflowException

--- src/main/java/com/mijecu25/dsa/Exception/QueueUnderflowException.java
+++ src/main/java/com/mijecu25/dsa/Exception/QueueUnderflowException.java
@@ -4,10 +4,10 @@ package com.mijecu25.dsa.Exception;
  * QueueUnderflowException meaning that the queue is empty.
  * 
  * @author Miguel Velez
- * @version 0.1.2.1
+ * @version 0.1.2.2
  */
-public class QueueUnderflowException extends RuntimeException 
-{
+public class QueueUnderflowException extends LinkedListUnderflowException {
+	
 	private static final long serialVersionUID = 4128603798523826231L;
 
 }
--- src/main/java/com/mijecu25/dsa/Exception/StackUnderflowException.java
+++ src/main/java/com/mijecu25/dsa/Exception/StackUnderflowException.java
@@ -4,9 +4,9 @@ package com.mijecu25.dsa.Exception;
  * StackUnderflowException meaning that the stack is empty.
  * 
  * @author Miguel Velez
- * @version 0.1.2.1
+ * @version 0.1.2.2
  */
-public class StackUnderflowException extends RuntimeException {
+public class StackUnderflowException extends LinkedListUnderflowException {
 
 	private static final long serialVersionUID = -8953227319152343757L;
 
