1415c042fd2aad232451e1ebb68de0c218ea435d
Miguel Velez <miguelvelez@mijecu25.com>
2015-07-21 12:49:25 -0400

    Tested the stack class

--- src/test/java/com/mijecu25/dsa/DataStructures/TestLinkedListStack.java
+++ src/test/java/com/mijecu25/dsa/DataStructures/TestLinkedListStack.java
@@ -8,90 +8,94 @@ import org.junit.Test;
  * This LinkedListStack test class.
  * 
  * @author Miguel Velez
- * @version 0.1.2.1
+ * @version 0.1.2.2
  */
 public class TestLinkedListStack {
-//	private int					value = 15;
-//	private LinkedListQueue 	queue = new LinkedListQueue();
-//	private LinkedListQueue		empty = new LinkedListQueue();
-//	
-//	@Before
-//	public void initialize() {
-//		this.queue.enqueue(value);
-//	}
-//	/**
-//	 * Test constructors.
-//	 */
-//	@Test
-//	public void testConstructors() {
-//		// Create queue
-//		LinkedListQueue queue = new LinkedListQueue();
-//		
-//		// Check that the queue was created
-//		Assert.assertNotNull(queue);
-//	}
-//
-//	/**
-//	 * Test is empty.
-//	 */
-//	public void isEmpty() {
-//		// Check if the queue is not empty
-//		Assert.assertFalse(this.queue.isEmpty());
-//		
-//		// Check if the empty queue is empty
-//		Assert.assertTrue(this.empty.isEmpty());
-//	}
-//	
-//	/**
-//	 * Test remove all.
-//	 */
-//	@Test
-//	public void testRemoveAll() {
-//		// Remove all
-//		this.queue.removeAll();
-//		
-//		// Check that the queue is empty
-//		Assert.assertTrue(this.queue.isEmpty());
-//	}
-//	
-//	/**
-//	 * Test peek.
-//	 */
-//	@Test
-//	public void testPeek() {
-//		// Check that the front of the queue is the value we inserted
-//		Assert.assertEquals(this.value, this.queue.peek());
-//	}
-//	
-//	/**
-//	 * Test dequeue.
-//	 */
-//	@Test
-//	public void testDequeue() {
-//		// Check that the dequeued value is the same as the value we inserted
-//		Assert.assertEquals(this.value, this.queue.dequeue());
-//	}
-//	
-//	/**
-//	 * Test enqueue by passing an object.
-//	 */
-//	public void testEnqueueObject() {
-//		// Enqueue a new value
-//		this.empty.enqueue(14);
-//		
-//		// Check that the new value was enqueued
-//		Assert.assertEquals(14, this.empty.peek());
-//	}
-//	
-//	/**
-//	 * Test enqueue by passing another queue
-//	 */
-//	public void testEnqueueQUeue() {
-//		// Join two queues
-//		this.empty.enqueue(this.queue);
-//		
-//		// Check that the empty queue is not empty anymore
-//		Assert.assertFalse(this.empty.isEmpty());
-//	}
+	private int					value = 15;
+	private LinkedListStack 	stack = new LinkedListStack();
+	private LinkedListStack		empty = new LinkedListStack();
+	
+	@Before
+	public void initialize() {
+		this.stack.push(value);
+	}
+	
+	/**
+	 * Test constructors.
+	 */
+	@Test
+	public void testConstructors() {
+		// Create stack
+		LinkedListStack stack = new LinkedListStack();
+		
+		// Check that the stack was created
+		Assert.assertNotNull(stack);
+	}
+
+	/**
+	 * Test is empty.
+	 */
+	@Test
+	public void isEmpty() {
+		// Check if the stack is not empty
+		Assert.assertFalse(this.stack.isEmpty());
+		
+		// Check if the empty stack is empty
+		Assert.assertTrue(this.empty.isEmpty());
+	}
+	
+	/**
+	 * Test remove all.
+	 */
+	@Test
+	public void testRemoveAll() {
+		// Remove all
+		this.stack.removeAll();
+		
+		// Check that the stack is empty
+		Assert.assertTrue(this.stack.isEmpty());
+	}
+	
+	/**
+	 * Test peek.
+	 */
+	@Test
+	public void testPeek() {
+		// Check that the top of the stack is the value we inserted
+		Assert.assertEquals(this.value, this.stack.peek());
+	}
+	
+	/**
+	 * Test pop.
+	 */
+	@Test
+	public void testPop() {
+		// Check that the poped value is the same as the value we inserted
+		Assert.assertEquals(this.value, this.stack.pop());
+	}
+	
+	/**
+	 * Test push by passing an object.
+	 */
+	@Test
+	public void testPushObject() {
+		// Push a new value
+		this.empty.push(14);
+		
+		// Check that the new value was pushed
+		Assert.assertEquals(14, this.empty.peek());
+	}
+	
+	/**
+	 * Test push by passing another stack
+	 */
+	@Test
+	public void testPushStack() {
+		// Join two stacks
+		this.empty.push(this.stack);
+		
+		// Check that the empty stack is not empty anymore
+		Assert.assertFalse(this.empty.isEmpty());
+	}
 
 }
