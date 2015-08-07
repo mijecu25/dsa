e16ae8ff94f88063284753d6c1e1a4f2ba008551
miguelvelezmj25 <miguelvelez@mijecu25.com>
2015-08-07 13:26:26 -0400

    Not implementing DS that are already in Java

--- src/main/java/com/mijecu25/dsa/exceptions/UnderflowException.java
+++ src/main/java/com/mijecu25/dsa/exceptions/UnderflowException.java
@@ -6,7 +6,7 @@ package com.mijecu25.dsa.exceptions;
  * @author Miguel Velez
  * @version 0.1.1.1
  */
-public class LinkedListUnderflowException extends RuntimeException {
+public class UnderflowException extends RuntimeException {
 
 	private static final long serialVersionUID = -4999175650453684735L;
 
