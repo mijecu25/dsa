50337ff9d8404d3ae40a394e710be6057c0ba24e
miguelvelezmj25 <miguelvelez@mijecu25.com>
2015-10-19 17:43:43 -0500

    Merging

--- src/test/java/com/mijecu25/dsa/algorithms/sort/inefficient/TestBogosort.java
+++ src/test/java/com/mijecu25/dsa/algorithms/sort/inefficient/TestBogosort.java
@@ -11,7 +11,7 @@ import com.mijecu25.dsa.algorithms.sort.inefficient.Bogosort;
  * This is the Bogosort test class.
  * 
  * @author Miguel Velez
- * @version 0.1.3.1
+ * @version 0.1.3.2
  */
 public class TestBogosort {
 	
@@ -34,4 +34,15 @@ public class TestBogosort {
 		Assert.assertTrue(Sort.isSorted(this.unsortedList));
 	}
 
+	/**
+	 * Test descending Bogosort
+	 */
+	@Test
+	public void testDescendingBogosort() {
+		// Sort the list using bogosort
+		Bogosort.sortDescending(this.unsortedList);
+
+		// Assert that the list is sorted in descending order
+		Assert.assertTrue(Sort.isReverseSorted(this.unsortedList));
+	}
 }
