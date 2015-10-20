2df9fa3510750d08f490b2517000581be92ae35e
miguelvelezmj25 <miguelvelez@mijecu25.com>
2015-10-20 09:39:04 -0500

    Testing Bogosort using Trivial Swap

--- src/test/java/com/mijecu25/dsa/algorithms/sort/inefficient/TestBogosort.java
+++ src/test/java/com/mijecu25/dsa/algorithms/sort/inefficient/TestBogosort.java
@@ -10,11 +10,12 @@ import com.mijecu25.dsa.algorithms.sort.Sort;
  * This is the Bogosort test class.
  * 
  * @author Miguel Velez
- * @version 0.1.3.3
+ * @version 0.1.3.4
  */
 public class TestBogosort {
 	
 	private int[] unsortedList = {5, 2, 24, 8, 15};
+	private double[] unsortedList2 = {5.2, 2.4, 24.2, 8.1, 15.5, 8.2};
 	
 	@Before
 	public void initialize() { ; }
@@ -42,4 +43,28 @@ public class TestBogosort {
 		// Assert that the list is sorted in descending order
 		Assert.assertTrue(Sort.isReverseSorted(this.unsortedList));
 	}
+	
+	/**
+     * Test Bogosort of doubles, floats, and elements
+     */
+    @Test
+    public void testBogosort2() {
+        // Sort the list using bogosort
+        Bogosort.sort(this.unsortedList2);
+        
+        // Assert that the list is sorted
+        Assert.assertTrue(Sort.isSorted(this.unsortedList2));
+    }
+
+    /**
+     * Test descending Bogosort of doubles, floats, and elements
+     */
+    @Test
+    public void testDescendingBogosort2() {
+        // Sort the list using bogosort
+        Bogosort.sortDescending(this.unsortedList2);
+
+        // Assert that the list is sorted in descending order
+        Assert.assertTrue(Sort.isReverseSorted(this.unsortedList2));
+    }
 }
