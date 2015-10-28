f21610b75ae5227c852df6aba1d20a5db344725b
miguelvelezmj25 <miguelvelez@mijecu25.com>
2015-10-28 17:13:14 -0500

    Initial commit of Search class and package

---
+++ src/main/java/com/mijecu25/dsa/algorithms/search/Search.java
@@ -0,0 +1,12 @@
+package com.mijecu25.dsa.algorithms.search;
+
+/**
+ * Search abstract class that implements different algorithms and utilities
+ * for searching.
+ * 
+ * @author Miguel Velez
+ * @version 0.1.3.1
+ */
+public abstract class Search {
+
+}
---
+++ src/test/java/com/mijecu25/dsa/algorithms/search/TestSearch.java
@@ -0,0 +1,16 @@
+package com.mijecu25.dsa.algorithms.search;
+
+import org.junit.Before;
+
+/**
+ * Test for Search class.
+ * 
+ * @author Miguel Velez
+ * @version 0.1.3.1
+ */
+public class TestSearch {
+
+    @Before
+    public void initialize() { ; }   
+    
+}
