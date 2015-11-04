847ea681d341552f0abd2e074d7cf521be231a15
miguelvelezmj25 <miguelvelez@mijecu25.com>
2015-11-03 20:45:13 -0600

    Organizing tests

--- src/test/java/com/mijecu25/dsa/algorithms/sort/inefficient/TestBogosort.java
+++ src/test/java/com/mijecu25/dsa/algorithms/sort/inefficient/TestBogosort.java
@@ -5,42 +5,91 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.mijecu25.dsa.algorithms.sort.Sort;
+import com.mijecu25.dsa.algorithms.sort.TestSort;
 
 /**
  * This is the Bogosort test class.
  * 
  * @author Miguel Velez - miguelvelezmj25
- * @version 0.1.3.6
+ * @version 0.1.3.7
  */
-public class TestBogosort {
-	
-	private int[]      unsortedArray = {5, 2, 24, 8, 15};
-	
-	@Before
-	public void initialize() { ; }
-	
-	/**
-	 * Test Bogosort
-	 */
-	@Test
-	public void testBogosort() {
-		// Sort the array using bogosort
-		Bogosort.sort(this.unsortedArray);
+public class TestBogosort extends TestSort {
 		
-		// Assert that the array is sorted
-		Assert.assertTrue(Sort.isSorted(this.unsortedArray));
-	}
-
+	@Before
+	public void initialize() { 
+	    super.initialize(); 
+    }
+	
 	/**
-	 * Test descending Bogosort
-	 */
-	@Test
-	public void testDescendingBogosort() {
-		// Sort the array using bogosort
-		Bogosort.sortDescending(this.unsortedArray);
+     * Test Bogosort primitives
+     */
+    @Test
+    public void testBogosort() {
+        // Sort the array using bogosort
+        Bogosort.sort(this.intArray1);
+        
+        // Assert that the array is sorted
+        Assert.assertTrue(Sort.isSorted(this.intArray1));
+    }
 
-		// Assert that the array is sorted in descending order
-		Assert.assertTrue(Sort.isReverseSorted(this.unsortedArray));
-	}
-	    
+    /**
+     * Test descending Bogosort primitives
+     */
+    @Test
+    public void testDescendingBogosort() {
+        // Sort the array using bogosort
+        Bogosort.sortDescending(this.intArray1);
+
+        // Assert that the array is sorted in descending order
+        Assert.assertTrue(Sort.isReverseSorted(this.intArray1));
+    }
+    
+    /**
+     * Test Bogosort elements
+     */
+    @Test
+    public void testBogosort2() {
+        // Sort the array using bogosort
+        Bogosort.sort(this.integerArray1);
+        
+        // Assert that the array is sorted
+        Assert.assertTrue(Sort.isSorted(this.integerArray1));
+    }
+
+    /**
+     * Test descending Bogosort elements
+     */
+    @Test
+    public void testDescendingBogosort2() {
+        // Sort the array using bogosort
+        Bogosort.sortDescending(this.integerArray1);
+
+        // Assert that the array is sorted in descending order
+        Assert.assertTrue(Sort.isReverseSorted(this.integerArray1));
+    }
+    
+    /**
+     * Test Bogosort list
+     */
+    @Test
+    public void testBogosort3() {
+        // Sort the array using bogosort
+        Bogosort.sort(this.list1);
+        
+        // Assert that the array is sorted
+        Assert.assertTrue(Sort.isSorted(this.list1));
+    }
+
+    /**
+     * Test descending Bogosort list
+     */
+    @Test
+    public void testDescendingBogosort3() {
+        // Sort the array using bogosort
+        Bogosort.sortDescending(this.list1);
+
+        // Assert that the array is sorted in descending order
+        Assert.assertTrue(Sort.isReverseSorted(this.list1));
+    }
+    	    
 }
--- src/test/java/com/mijecu25/dsa/algorithms/sort/quadratic/TestInsertion.java
+++ src/test/java/com/mijecu25/dsa/algorithms/sort/quadratic/TestInsertion.java
@@ -5,42 +5,91 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.mijecu25.dsa.algorithms.sort.Sort;
+import com.mijecu25.dsa.algorithms.sort.TestSort;
 
 /**
  * This is the Insertion sort test class.
  * 
  * @author Miguel Velez - miguelvelezmj25
- * @version 0.1.3.6
+ * @version 0.1.3.7
  */
-public class TestInsertion {
-	
-	private int[] unsortedArray = {5, 2, 24, 8, 15};
-	
+public class TestInsertion extends TestSort {
+		
 	@Before
-	public void initialize() { ; }
+	public void initialize() { 
+	    super.initialize();
+	}
 	
 	/**
-	 * Test Insertion
+	 * Test Insertion primitives
 	 */
 	@Test
 	public void testInsertion() {
 		// Sort the array using insertion
-		Insertion.sort(this.unsortedArray);
+		Insertion.sort(this.intArray1);
 		
 		// Assert that the array is sorted
-		Assert.assertTrue(Sort.isSorted(this.unsortedArray));
+		Assert.assertTrue(Sort.isSorted(this.intArray1));
 	}
 
 	/**
-	 * Test descending Insertion
+	 * Test descending Insertion primitives
 	 */
 	@Test
 	public void testDescendingInsertion() {
 		// Sort the array using insertion
-		Insertion.sortDescending(this.unsortedArray);
+		Insertion.sortDescending(this.intArray1);
 
 		// Assert that the array is sorted in descending order
-		Assert.assertTrue(Sort.isReverseSorted(this.unsortedArray));
+		Assert.assertTrue(Sort.isReverseSorted(this.intArray1));
 	}
+	
+	   /**
+     * Test Insertion elements
+     */
+    @Test
+    public void testInsertion2() {
+        // Sort the array using insertion
+        Insertion.sort(this.integerArray1);
+        
+        // Assert that the array is sorted
+        Assert.assertTrue(Sort.isSorted(this.integerArray1));
+    }
+
+    /**
+     * Test descending Insertion elements
+     */
+    @Test
+    public void testDescendingInsertion2() {
+        // Sort the array using insertion
+        Insertion.sortDescending(this.integerArray1);
+
+        // Assert that the array is sorted in descending order
+        Assert.assertTrue(Sort.isReverseSorted(this.integerArray1));
+    }
+    
+    /**
+     * Test Insertion list
+     */
+    @Test
+    public void testInsertion3() {
+        // Sort the array using insertion
+        Insertion.sort(this.list1);
+        
+        // Assert that the array is sorted
+        Assert.assertTrue(Sort.isSorted(this.list1));
+    }
+
+    /**
+     * Test descending Insertion list
+     */
+    @Test
+    public void testDescendingInsertion3() {
+        // Sort the array using insertion
+        Insertion.sortDescending(this.list1);
+
+        // Assert that the array is sorted in descending order
+        Assert.assertTrue(Sort.isReverseSorted(this.list1));
+    }
 	    
 }
